ae92e3ef1869767c17707ef4cd09f2214f9759f6
80839211 <116695532+80839211@users.noreply.github.com>
2023-04-18 20:54:05 +0000

    Day02 - added data types

--- src/Day02/day2.py
+++ src/Day02/day2.py
@@ -6,3 +6,10 @@ Date:       17/04/2023
 --------------------------
 """
 
+# data type:
+
+int_num = 23
+float_num = 23.01
+str_name = "Akshay"
+
+print('integer value:', int_num, 'float value:',float_num,'string:',str_name) 
