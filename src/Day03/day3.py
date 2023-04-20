653b1c901dbccd54806aaf7eb573b05139d87bd6
80839211 <116695532+80839211@users.noreply.github.com>
2023-04-20 14:28:16 +0000

    Day03 - Arithmatic ops

---
+++ src/Day03/day3.py
@@ -0,0 +1,28 @@
+"""
+Day 2 of python learning
+--------------------------
+Author:     Akshay
+Date:       17/04/2023
+--------------------------
+"""
+
+# Arithmetic operators:
+
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+
+print("first value is:", a , "| second value is:", b)
+
+add = a + b
+sub = a - b
+mult = a * b
+div = a / b
+mod = a % b
+
+print("=======================================")
+print("Sum of above two values: ", add)
+print("Substraction of above two values: ", sub)
+print("Multiplication of above two values: ", mult)
+print("Divison of above two values: ", div)
+print("Modulo of above two values: ", mod)
+print("=======================================")
