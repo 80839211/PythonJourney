2ae6111e782470ca93bf1d572bb2a0c823618c8c
80839211 <116695532+80839211@users.noreply.github.com>
2023-04-22 04:29:52 +0000

    Day04 - User input

---
+++ src/Day05/day5.py
@@ -0,0 +1,16 @@
+"""
+Day 5 of python learning
+--------------------------
+Author:     Akshay
+Date:       21/04/2023
+--------------------------
+"""
+
+# taking user input in python:
+
+# using input() to take user input
+num = input('Enter a number: ')
+
+print('You Entered:', num)
+
+print('Data type of num:', type(num))
