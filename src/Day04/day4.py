59e00b84f97b6684bd3e3550fbf75f19fda870d6
80839211 <116695532+80839211@users.noreply.github.com>
2023-04-22 04:20:16 +0000

    added day4 file with type conversion

---
+++ src/Day04/day4.py
@@ -0,0 +1,29 @@
+"""
+Day 4 of python learning
+--------------------------
+Author:     Akshay
+Date:       20/04/2023
+--------------------------
+"""
+
+# type casting in python:
+
+#
+str = "23"
+num = 27
+
+# Converting the string into an integer number.
+strtonum = int(str)
+
+sum = num + strtonum
+print("The Sum of both the numbers is: ", sum)
+
+
+
+salary = 25000
+bonus = salary * (0.25)
+
+# Converting the salary into floating point number.
+new_salary = float(salary)
+print("Salary converted into floating-point: ", new_salary)
+print("Salary after adding bonus: ", new_salary + bonus)
