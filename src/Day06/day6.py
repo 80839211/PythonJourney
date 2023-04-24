4ea1f2f9efe983fdb3a968726ead4c8e2bba1094
80839211 <116695532+80839211@users.noreply.github.com>
2023-04-24 05:33:11 +0000

    Day06 - Dictionary data type

---
+++ src/Day06/day6.py
@@ -0,0 +1,18 @@
+"""
+Day 6 of python learning (Excercise)
+--------------------------
+Author:     Akshay
+Date:       232/04/2023
+--------------------------
+"""
+
+# Dictonary 
+
+some_dict = {"akshay": 23, "john": 20, "hrithik": 17, "varun": 14}
+
+def print_dict(dictionary):
+	for i, j in dictionary.items():
+		print(f'{i} : {j}', end=" | ")
+		# print('\n{}: {}'.format(i, j))	
+
+print_dict(some_dict)
